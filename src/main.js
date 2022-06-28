cfff29d6e484b382a091e80d9ac3a657b9e0119d
basta <jsembasta@gmail.com>
2022-06-29 01:01:16 +0200

    background and fixes and new buttons and about

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ function sample(array) {
 createApp({
     data() {
         return {
+            lastIncorrect: false,
+            lastCorrect: false,
             asking: true,
             correct: 0,
             incorrect: 0,
@@ -30,19 +32,26 @@ createApp({
         select(event) {
             if (!this.asking) {
                 this.loadTest()
+                this.lastCorrect = false;
+                this.lastIncorrect = false;
             } else {
                 let buttonId = event.target.id
                 if ((buttonId === "left" && this.turtest.leftpost.isHuman)
                     || (buttonId === "right" && this.turtest.rightpost.isHuman)) {
                     this.correct += 1;
+                    this.lastCorrect = true;
                 } else {
                     this.incorrect += 1;
+                    this.lastIncorrect = true;
                 }
             }
             this.asking = !this.asking
         },
         loadTest() {
-            fetch(window.location + "turtests.json").then(response => response.json()).then(json => {
+            console.log(window.location + "turtests.json")
+            console.log("http://" + window.location.host + window.location.pathname + "turtests.json")
+            let url = "http://" + window.location.host + window.location.pathname + "turtests.json";
+            fetch(url).then(response => response.json()).then(json => {
                 let test = sample(json)
                 this.turtest.subreddit = test.subreddit
                 console.log(test)
